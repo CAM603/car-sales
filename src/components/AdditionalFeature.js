9b5bae1b8db53d8f132ff77a02ee4fab780a66d0
CAM603 <cameronhawley@comcast.net>
2020-01-22 18:00:09 -0500

    added bulma buttons

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -13,7 +13,7 @@ const AdditionalFeature = props => {
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
       <button 
-      className="button"
+      className="button is-primary"
       onClick={() => buyItem(props.feature)}
       >Add</button>
       {props.feature.name} (+{props.feature.price})
